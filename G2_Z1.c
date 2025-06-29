58a8be2c4648eb60a8f24418fbfa38d5df19aae4
Fiedor <stachfie@wp.pl>
2025-06-29 19:13:32 +0200

    done

---
+++ G2_Z1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct element {
+    double e;
+    struct element *next;
+} NODE;
+typedef struct {
+    NODE *front;
+    NODE *rear;
+} kolejka;
+void enqueue(kolejka *q, double value) {
+    NODE *new_element = (NODE *)malloc(sizeof(NODE));
+    if (!new_element) {
+        printf("Błąd alokacji pamięci!\n");
+        return;
+    }
+    new_element->e = value;
+    new_element->next = NULL;
+    if (q->front == NULL) {
+        q->front = new_element;
+        q->rear  = new_element;
+    }
+    else {
+        q->rear->next = new_element;
+        q->rear = new_element;
+    }
+    printf("Dodano %f do kolejki.\n", value);
+}
+void dequeue(kolejka *q) {
+    if (q->front == NULL) {
+        printf("Kolejka jest pusta.\n");
+        return;
+    }
+    NODE *temp = q->front;
+    printf("Usunięto: %f\n", temp->e);
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    free(temp);
+}
